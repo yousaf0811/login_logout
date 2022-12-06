3900ae6c14b2a7265ce72cb5972db992704469aa
yousaf0811 <yousaf0811@gmail.com>
2022-12-06 06:34:31 +0500

    not complete yet

--- src/components/login.js
+++ src/components/login.js
@@ -1,20 +1,58 @@
+import React, { useEffect } from "react";
+
+import { useState } from "react";
 
 import {useNavigate} from "react-router-dom" 
-
 const Login = ()=>{
-    let navigate = useNavigate() 
-    // const [email,setEmail] = useState('');
-    // const [pass,setPass] = useState('');
+    const [database, setDatabase] = useState([]);
+    const navigate = useNavigate();
+    const handleClick = () => {
+      navigate("/home");
+    };
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const { email, password } = document.forms[0];
+        const userData = database.find((user) => user.email === email.value);
+        if (userData) {
+          if (userData.password !== password.value) {
+            alert("Invalid password");
+          } else {
+            localStorage.setItem("name", JSON.stringify(userData.name));
+            localStorage.setItem("id", JSON.stringify(userData.id));
+            handleClick();
+          }
+        } else {
+          alert("email not found");
+        }
+        console.log(password, email);
+        if (!email.value || !password.value) {
+          alert("Fill the empty input fields");
+          return false;
+        }
+      };
+
+
+
+    useEffect(() => {
+        let localData = localStorage.getItem("name");
+        console.log("Data :: ", localData);
+        fetch("http://localhost:3003/users")
+          .then((resp) => resp.json())
+          .then((info) => setDatabase(info));
+      }, []);
+    // let navigate = useNavigate() 
     return(
         <>
         <div className="form">
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit} >
             <h2>Login</h2>
             <label for="email" >Enter the Email : </label>
             <input type='email' placeholder="Email" id="email" name="email" ></input>
             <label for="password" >Enter the Password : </label>
             <input type='password' placeholder="********" id="password" name="password" ></input>
-            <button type="submit" onClick ={()=>{ navigate("/home")}} >Log In</button>
+            <button type="submit" >Log In</button>
         </form>
         <button className="link-btn" onClick ={()=>{ navigate("/register")}} >Don't have any Account? Reguister?</button>
         </div>
--- src/components/register.js
+++ src/components/register.js
@@ -1,18 +1,39 @@
-// import { useState } from "react"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-
-
 const Register = () => {
     let navigate = useNavigate()
     const [id, setId] = useState(0);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [DOB, setDOB] = useState('');
-    const [gender, setGender] = useState('');
-
-    function updateData() {
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [pass, setPass] = useState(null);
+    const [DOB, setDOB] = useState(null);
+    const [gender, setGender] = useState(null);
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        if(id === "id"){
+            setId(value);
+        }
+        if (id === "name") {
+          setName(value);
+        }
+        if (id === "email") {
+            setEmail(value);
+          }
+        if (id === "password") {
+          setPass(value);
+        }
+        if(id === "DOB"){
+            setDOB(value);
+        }
+        if(id === "gender"){
+            setGender(value);
+        }
+      };
+      const handleClick = () => {
+        navigate('/home')};
+      const handleSubmit = () => {
+        console.log(name);
+        if(!name ) { alert('Fill the empty input fields');  return false}; 
         const user = {
             id,
             name,
@@ -20,37 +41,37 @@ const Register = () => {
             pass,
             DOB,
             gender,
-        };
-    localStorage.setItem('users', JSON.stringify(user));
-    fetch('http://localhost:3002/users', {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(user),
-        })
-        .then((res) => res.json())
-        .then((info) => console.log(info));
-    };
+        }
+    localStorage.setItem('name', JSON.stringify(user.name));
+        fetch('http://localhost:3003/users',{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(user)
+        }).then(resp => resp.json()).then(info => handleClick() );
+      };
+
     return (
         <div className="form">
-            <form className="register-form" onSubmit={updateData} >
+            <form className="register-form">
                 <h2>Register</h2>
                 <label>ID</label>
-                <input onChange={(event1) => setId(event1.target.value)} type="number" id="id" name="id" placeholder="Enter ID" ></input>
+                <input onChange={(e) => handleInputChange(e)} type="number" id="id" name="id" placeholder="Enter ID" ></input>
                 <label>Full Name</label>
-                <input onChange={(event1) => setName(event1.target.value)} type="name" id="name" name="name" placeholder="Full Name" ></input>
+                <input onChange={(e) => handleInputChange(e)} type="name" id="name" name="name" placeholder="Full Name" ></input>
                 <label for="email" >Enter the Email : </label>
-                <input onChange={(event1) => setEmail(event1.target.value)} type='email' placeholder="Email" id="email" name="email" ></input>
+                <input onChange={(e) => handleInputChange(e)} type='email' placeholder="Email" id="email" name="email" ></input>
                 <label for="password" >Enter the Password : </label>
-                <input onChange={(event1) => setPass(event1.target.value)} type='password' placeholder="********" id="password" name="password" ></input>
+                <input onChange={(e) => handleInputChange(e)} type='password' placeholder="********" id="password" name="password" ></input>
                 <label>DOB</label>
-                <input onChange={(event1) => setDOB(event1.target.value)} type="date" id="DOB" name="DOB" placeholder="Enter the DOB" ></input>
+                <input onChange={(e) => handleInputChange(e)} type="date" id="DOB" name="DOB" placeholder="Enter the DOB" ></input>
                 <label>Gender</label>
-                <input onChange={(event1) => setGender(event1.target.value)} type="name" id="gender" name="gender" placeholder="Enter the Gender" ></input>
-
-                <button type="submit" >Register</button>
+                <input onChange={(e) => handleInputChange(e)} type="name" id="gender" name="gender" placeholder="Enter the Gender" ></input>
             </form>
+            <button
+                 onClick={() => handleSubmit()}   
+                 >Register</button>
             <button className="link-btn" onClick={() => { navigate("/") }} >Already have Account? Then Log in Here.</button>
         </div>
 )
