9417345b9ab5445b84359af54d73c8b149929fab
yousaf0811 <yousaf0811@gmail.com>
2022-12-06 06:38:01 +0500

    not complete

--- src/App.js
+++ src/App.js
@@ -18,8 +18,7 @@ function App() {
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login/> } /> 
-            <Route path="/register" element={<Register/> } /> 
-            
+            <Route path="/register" element={<Register/> } />    
             <Route path="/home" element={<Home/> } />
             <Route path="/details" element={<Details/> } />  
         </Routes>
